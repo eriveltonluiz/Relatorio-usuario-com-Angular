83f6f0a7c766c975a754ef6515a7cf10d5beef21
eriveltonluiz <erivelton-paulista@hotmail.com>
2021-03-04 22:46:05 -0300

    Ajustando integração com API parte 4

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://api-relatorio-usuario.herokuapp.com/projetospringrestapi'
+  baseUrl: 'https://api-relatorio-usuario.herokuapp.com/projetospringrestapi/'
 };
